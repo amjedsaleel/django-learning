48ec49e038aef3ba72d6e88895e7689cc5f2a383
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-16 16:46:50 +0530

    one to one relationship

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Phone(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_no = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.phone_no
+
+
+
