d594a127991d4461f2f97b33434cdc5b9cd9a388
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-16 21:13:12 +0530

    logout

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 
+@login_required(login_url='login')
 def index(request):
     return render(request, 'home.html')
 
