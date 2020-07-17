6887a58f1dbe5c084ff7da5c6b40f5bdc3d5a83b
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-17 14:01:52 +0530

    ajax

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
--- core/views.py
+++ core/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+from .forms import ContactModelForm
 
 
 def index(request):
-    return HttpResponse('Hello...')
+    form = ContactModelForm()
+    # if request.method == 'POST':
+    #     form = ContactModelForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('/')
 
+    if request.is_ajax():
+        form = ContactModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({
+                'msg': 'Success'
+            })
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'index.html', context)
