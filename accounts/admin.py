42fa777cb248146ca847d01a8a56e06fc1b7e4c2
Amjed Saleel K <amjedsaleel@gmail.com>
2020-12-11 22:04:27 +0530

    user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.models import Account
 
 # Register your models here.
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin')
+
+
+admin.site.register(Account)
