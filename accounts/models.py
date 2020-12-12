3d9d97a1a6284f851cef45ed82e02846929e89c5
Amjed Saleel K <amjedsaleel@gmail.com>
2020-12-12 14:17:44 +0530

    something

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import User
 
 
 class CustomAccountManager(BaseUserManager):
