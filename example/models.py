ee2639f9878c2acf0a22a03f712034f6e86b788e
Amjed Saleel K <amjedsaleel@gmail.com>
2021-02-15 22:37:34 +0530

    final

--- example/models.py
+++ example/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Example(models.Model):
+    name = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
--- example/views.py
+++ example/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms import modelformset_factory
+from . models import Example
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    example_formset = modelformset_factory(Example, fields=('name', 'location'), extra=2)
+
+    if request.method == 'POST':
+        print('post')
+        form = example_formset(request.POST)
+        if form.is_valid():
+
+            instances = form.save(commit=False)
+            for instance in instances:
+                instance.save()
+
+            print('valid')
+            # form.save()
+            return redirect('index')
+    print('get')
+    form = example_formset(queryset=Example.objects.none())
+
+    return render(request, 'index.html', {'form': form})
