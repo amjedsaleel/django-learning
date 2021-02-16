6f107a3be5239c1444af5054e1af25c7093c00ee
Amjed Saleel K <amjedsaleel@gmail.com>
2021-02-16 22:29:03 +0530

    .

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.PositiveIntegerField()
+    programmer = models.ForeignKey(Programmer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} " " {self.programmer}'
--- core/views.py
+++ core/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Language, Programmer
+from django.forms import modelformset_factory, inlineformset_factory
+
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'index.html')
+def index(request, programmer_id):
+    programmer = Programmer.objects.get(pk=programmer_id)
+    # language_formset = modelformset_factory(Language, fields=('name',))
+    language_formset = inlineformset_factory(Programmer, Language, fields=(
+        'name',), extra=2, validate_min=3,)  # Parent model, Chilled model (Programmer, Language), field name is of parent
+
+    if request.method == 'POST':
+        # formset = language_formset(request.POST, queryset=Language.objects.filter(programmer__id=programmer.id))
+        formset = language_formset(request.POST, instance=programmer)
+
+        if formset.is_valid():
+            formset.save()
+            # instances = formset.save(commit=False)
+
+            # for instance in instances:
+            #     instance.programmer_id = programmer.id
+            #     instance.save()
+            #     return redirect('index', programmer_id=programmer_id)
+
+    # formset = language_formset(queryset=Language.objects.filter(programmer__id=programmer.id))
+    formset = language_formset(instance=programmer)
+
+    return render(request, 'index.html', {'formset': formset, 'programmer_id': programmer_id})
