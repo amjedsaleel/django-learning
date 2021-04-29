d6403272c664294d2d21016889db1d8ee2e499d7
Amjed Saleel K <amjedsaleel@gmail.com>
2021-04-29 23:21:55 +0530

    image delete form media root

--- core/models.py
+++ core/models.py
@@ -8,3 +8,13 @@ User = settings.AUTH_USER_MODEL
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.PositiveIntegerField()
+    image = models.ImageField()
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, null=True)
+    liked = models.ManyToManyField(User, blank=True)
+    notify_users = models.BooleanField(default=False)
+    notify_users_timestamp = models.DateTimeField(blank=True, null=True, auto_now_add=False)
+    active = models.BooleanField(default=True)
--- core/signals.py
+++ core/signals.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save, pre_delete, post_delete
 from django.dispatch import receiver
-from . models import Profile
+from django.utils.text import slugify
+from . models import Profile, BlogPost
 
 User = settings.AUTH_USER_MODEL
 
@@ -15,5 +16,22 @@ def create_profile(sender, instance, created, *args, **kwargs):
 
 @receiver(post_save, sender=User)
 def update_profile(sender, instance, created, *args, **kwargs):
+    """
+    After User object used in database (past_save)
+    """
     if not created:
         print("Profile updated")
+
+
+@receiver(pre_save, sender=BlogPost)
+def blog_handler(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+
+@receiver(pre_delete, sender=Profile)
+def profile_delete_handler(sender, instance, *args, **kwargs):
+    print("Profile will delete")
+    print(sender.objects.get(id=instance.id), "..........................")
+    instance.image.delete()
+
